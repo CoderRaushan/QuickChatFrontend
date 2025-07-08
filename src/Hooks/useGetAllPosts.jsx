558c4726af18ca8502e230de02f3e9f96b9916d7
CoderRaushan <raushankumar23082004@gmail.com>
2025-07-08 18:17:03 +0530

    fix building by importing in correct manner of a hook

--- src/Hooks/useGetAllPosts.jsx
+++ src/Hooks/useGetAllPosts.jsx
@@ -1,3 +1,34 @@
+
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { setPosts } from "../ReduxStore/PostSlice.js";
+
+function useGetAllPosts() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchInitialPosts = async () => {
+      const MainUri = import.meta.env.VITE_MainUri;
+      try {
+        const res = await axios.get(`${MainUri}/user/post/all?skip=0&limit=5`, {
+          withCredentials: true,
+        });
+
+        if (res.data.success) {
+          dispatch(setPosts(res.data.posts)); // sirf first 5 posts set karo
+        } else {
+          toast.error(res.data.message || "Fetch failed");
+        }
+      } catch (err) {
+        toast.error(err.response?.data?.message || "Server error");
+      }
+    };
+
+    fetchInitialPosts();
+  }, [dispatch]);
+}
+export default useGetAllPosts;
 // import axios from 'axios'
 // import React, { useEffect } from 'react'
 // import { useDispatch } from 'react-redux';
@@ -36,32 +67,3 @@
 
 // export default useGetAllPosts;
 // src/hooks/useGetAllPosts.js
-import axios from "axios";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { toast } from "react-toastify";
-import { setPosts } from "../ReduxStore/PostSlice.js";
-
-export default function useGetAllPosts() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const fetchInitialPosts = async () => {
-      const MainUri = import.meta.env.VITE_MainUri;
-      try {
-        const res = await axios.get(`${MainUri}/user/post/all?skip=0&limit=5`, {
-          withCredentials: true,
-        });
-
-        if (res.data.success) {
-          dispatch(setPosts(res.data.posts)); // sirf first 5 posts set karo
-        } else {
-          toast.error(res.data.message || "Fetch failed");
-        }
-      } catch (err) {
-        toast.error(err.response?.data?.message || "Server error");
-      }
-    };
-
-    fetchInitialPosts();
-  }, [dispatch]);
-}
